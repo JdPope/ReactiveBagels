55d1fd95f382aafbcc8bb4cf919a4a2deea1fd80
Jessica Pope <jessica.pope.co@gmail.com>
2020-01-27 16:19:47 -0700

    Render bagel from bagel component

--- reactbagels/src/BagelContainer.js
+++ reactbagels/src/BagelContainer.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import Bagel from './bagel'
+
 
 function BagelContainer(props){
-  // const bagelItem = (props) => {
-  //   bagels.map(bagel =>{
-  //     <Bagel/>
-  //   })
-  // console.log(props)
+  const bagelItem = props.bagels.map(bagel => <Bagel bagel={bagel.type}/>)
   return(
     <div>
       <h2>This is the Bagel Container</h2>
+      {bagelItem}
     </div>
   )
 
---
+++ reactbagels/src/bagel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Bagel(props){
+  return(
+    <div>
+      <h3>Bagel Type</h3>
+      <p>{props.bagel}</p>
+    </div>
+  )
+}
+
+export default Bagel

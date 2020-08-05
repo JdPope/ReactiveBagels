43108da1f8bc7e3a81dcdcc1e056ebaa1bb1cb88
Jessica Pope <jessica.pope.co@gmail.com>
2020-08-04 21:31:32 -0600

    Complete Form

--- reactbagels/src/App.js
+++ reactbagels/src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import BagelContainer from './BagelContainer'
 import BagelForm from './BagelForm'
-
+const BASE_URL = 'http://bagel-api-fis.herokuapp.com/bagels'
 class App extends Component {
 
   state = {
@@ -10,7 +10,7 @@ class App extends Component {
   }
 
   componentDidMount(){
-    fetch('http://bagel-api-fis.herokuapp.com/bagels')
+    fetch(BASE_URL)
       .then(response => response.json())
       .then(result => {
         this.setState({
@@ -19,11 +19,25 @@ class App extends Component {
       })
   }
 
+  makeBagel = bagel => {
+    console.log(bagel)
+    return fetch(BASE_URL, {
+      method:'POST',
+      headers:{
+        'Accept':'application/json',
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify(bagel)
+    }).then(this.parseJSON)
+      .then(bagel => this.setState({ bagels: [...this.state.bagels, bagel] }))
+  }
+
+
   render(){
   return (
     <div className="App">
       <h1>Reactive Bagels</h1>
-      <BagelForm/>
+      <BagelForm makeBagel={this.makeBagel}/>
       <BagelContainer bagels={this.state.bagels}/>
     </div>
   );
--- reactbagels/src/BagelForm.js
+++ reactbagels/src/BagelForm.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function BagelForm(){
-  return(
-    <div>
-      <h2>This is the BagelForm</h2>
-    </div>
-  )
+class BagelForm extends Component{
+
+    state = {
+        type:'default bagel'
+      }
+    
+    handleChange = event => {
+      this.setState({
+        [event.target.name]: event.target.value
+      })
+    }
+  
+    handleSubmit = event => {
+      event.preventDefault()
+      this.props.makeBagel(this.state)
+    }
+
+    render(){
+    return (
+        <>
+        <h2>This is the Bagels Form</h2>
+        <form onSubmit={this.handleSubmit}>
+        <label>
+            Make a Bagel!
+            <input id="bagel" name="type" type="text" value={this.state.type} onChange={this.handleChange} />
+          </label>
+          <button type="submit">Make Bagel!</button>
+        </form>
+        </>
+    )
+    }
 }
 
 export default BagelForm
